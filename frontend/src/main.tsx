98dd12962f52b8b6f8d7123fd31d07a7d3075971
Dave Bunyan <davebunyan1@gmail.com>
2026-08-30 10:57:32 -0400

    Split ThemeContext and ThemeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "./contexts/ThemeContext.tsx";
+import { ThemeProvider } from "./contexts/ThemeProvider.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
